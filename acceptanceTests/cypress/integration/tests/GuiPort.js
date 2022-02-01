73ee60f4f2d1bd0be090fcd19f4f3bbf96e78519
Adam Kol <adam@up9.com>
2022-02-01 16:22:34 +0200

    no message

--- acceptanceTests/cypress/integration/tests/GuiPort.js
+++ acceptanceTests/cypress/integration/tests/GuiPort.js
@@ -1,11 +1,14 @@
+import {findLineAndCheck, getExpectedDetailsDict} from "../testHelpers/StatusBarHelper";
+
 it('check', function () {
+    const podName = 'httpbin', namespace = 'mizu-tests';
+
     cy.intercept('GET', 'http://localhost:8898/status/tap', { statusCode: 200 }).as('statusTap');
-    cy.intercept('ws://localhost:8898/ws').as('ws');
 
     cy.visit(`http://localhost:8898/`);
 
     cy.wait('@statusTap');
-    cy.wait('@ws')
+    findLineAndCheck(getExpectedDetailsDict(podName, namespace));
 
     cy.get('.header').should('be.visible');
     cy.get('.TrafficPageHeader').should('be.visible');
